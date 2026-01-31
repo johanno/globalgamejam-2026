6195bd6815818ca6a5c8cb95b1ab8ca88df1a681
johanno <johannes@johannes.com>
2026-01-31 13:06:16 +0100

    move

--- player.gd
+++ player.gd
@@ -12,11 +12,11 @@ func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	elif Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	elif Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
