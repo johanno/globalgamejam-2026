c99dd6ed3875e556a94fcbdf5aeb39d651c1fe27
MrLetsplay <mr.letsplay2003@gmail.com>
2026-01-31 14:01:25 +0100

    Add quit_game action, Use physics for movement

--- player.gd
+++ player.gd
@@ -2,16 +2,21 @@ extends CharacterBody2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var tile_size: Vector2i # Size of one tile
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var tile_map = get_tree().root.get_node("TileScene/TileMapLayer") as TileMapLayer
+	tile_size = tile_map.tile_set.tile_size
 	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	print("DELTA", delta)
-	#var velocity = Vector2.ZERO # The player's movement vector.
 	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("quit_game"):
+		get_tree().quit()
+	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	elif Input.is_action_pressed("move_left"):
@@ -23,22 +28,21 @@ func _physics_process(delta: float) -> void:
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		#$AnimatedSprite2D.play()
+		$AnimatedSprite2D.play()
 	else:
 		pass
-		#$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.stop()
 		
 	#position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, screen_size)
+	position = position.clamp(Vector2.ZERO, screen_size)
 	var collision = move_and_slide()
-	print(collision)
 	if velocity.x != 0:
 		pass
-		#$AnimatedSprite2D.animation = "walk"
-		#$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
-		#$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		pass
-		#$AnimatedSprite2D.animation = "up"
-		#$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
