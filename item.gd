c043b6df0c73240d477903fbb1380bd699a3a4b6
MrLetsplay <mr.letsplay2003@gmail.com>
2026-01-31 21:14:59 +0100

    Merge

--- item.gd
+++ item.gd
@@ -3,5 +3,5 @@ extends Node2D
 var initial_pos: Vector2
 
 func _process(delta: float) -> void:
-	var offset = Vector2(0, sin(Time.get_ticks_msec()) * 20)
-	#$Sprite.transform.position = offset
+	var offset = Vector2(0, sin(Time.get_ticks_msec() / 500.0) * 5)
+	$Sprite.transform = Transform2D(0, offset)
