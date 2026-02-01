9c410fc27b24c09173c90940ec9d8ea967d4a375
MrLetsplay <mr.letsplay2003@gmail.com>
2026-02-01 15:10:08 +0100

    Rescale level4

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -30,7 +30,7 @@ func _physics_process(delta: float) -> void:
 	var direction: Vector2i = (player.get_current_tile() - get_current_tile()).clampi(-1,1)
 	if direction == Vector2i(0,0):
 		player.die()
-		
+
 	velocity = Vector2.ZERO
 	if remaining_move_delay > 0:
 		remaining_move_delay -= delta
